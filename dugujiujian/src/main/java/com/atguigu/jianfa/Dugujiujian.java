e397ebfec4371a9ba651f773d172130e31e98f11
yuebuqun <yuebuqun777888@126.com>
2018-12-05 15:24:35 +0800

    33333

--- dugujiujian/src/main/java/com/atguigu/jianfa/Dugujiujian.java
+++ dugujiujian/src/main/java/com/atguigu/jianfa/Dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 
     public static void main(String[] args) {
-        System.out.println("2222222222222222222222222222");
+        System.out.println("33333333333333333");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
