efd1e887635165fafc3fc07046fb42bd5e4b896a
zhangchen <windyzj@163.com>
2018-12-05 15:22:32 +0800

    22222

--- dugujiujian/src/main/java/com/atguigu/jianfa/Dugujiujian.java
+++ dugujiujian/src/main/java/com/atguigu/jianfa/Dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 
     public static void main(String[] args) {
-        System.out.println("11111111111111111111111111111111111111111111111");
+        System.out.println("2222222222222222222222222222");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
         System.out.println("hello dugujiujian!!!!!!!!!!!!!");
